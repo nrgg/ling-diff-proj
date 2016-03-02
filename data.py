c39f2eeac54cec54f9be362227c280b6090d9502
Nicholas Gow <nicholasrgow@gmail.com>
2016-03-02 11:00:25 +0100

    data paths

--- data.py
+++ data.py
@@ -1,10 +1,11 @@
 import scipy.io as io
 import pandas as pd
 import numpy as np
+import os
 
 
 def get_XYV():
-    mat = io.loadmat("demo-data-distrib/demo-data-distrib.mat")
+    mat = io.loadmat(os.path.join("demo-data-distrib", "demo-data-distrib.mat"))
     columns = [x[0] for x in mat["header"][0]]
     Y = pd.DataFrame(mat["census"], columns=columns)
     V = pd.DataFrame([x[0] for x in mat["vocab"].T[0]], columns=["token"])
@@ -13,7 +14,7 @@ def get_XYV():
 
 def load_wvs(V, dim=50):
     tokens = dict(zip(V.token, V.index))
-    wv_path = "/home/nrg/datasets/glove.twitter.27B.{dim}d.txt".format(dim=dim)
+    wv_path = os.path.join("word-vectors","glove.twitter.27B.{dim}d.txt".format(dim=dim))
     wvs = np.zeros((len(tokens), dim))
     has_wv = [False] * len(tokens)
     for line in open(wv_path):
